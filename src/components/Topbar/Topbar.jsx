a4e26f55cc1bbf18c2280e45a7c8fdb4ad1c4d5b
muhmmdfrd <mfarid2121@gmail.com>
2020-09-13 05:08:34 +0700

    delete photo profile

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,40 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Topbar = () => {
   return (
-    <nav className='navbar navbar-expand navbar-light bg-white topbar static-top shadow'>
-      <ul className='navbar-nav ml-auto'>
-        <li className='nav-item dropdown no-arrow'>
+    <nav className="navbar navbar-expand navbar-light bg-white topbar static-top shadow">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item dropdown no-arrow">
           <a
-            className='nav-link dropdown-toggle'
-            href='/#'
-            id='userDropdown'
-            role='button'
-            data-toggle='dropdown'
-            aria-haspopup='true'
-            aria-expanded='false'
+            className="nav-link dropdown-toggle"
+            href="/#"
+            id="userDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
           >
-            <span className='mr-2 d-none d-lg-inline text-gray-600 small'>
-              {window.localStorage.getItem('_uin')}
+            <span className="mr-2 d-none d-lg-inline text-gray-600 small">
+              {window.localStorage.getItem("_uin")}
+              <i style={{ marginLeft: 4 }} className="fa fa-caret-down"></i>
             </span>
-            <img
-              className='img-profile rounded-circle'
-              src='https://source.unsplash.com/QAB-WJcbgJk/60x60'
-              alt='profile'
-            />
           </a>
 
           <div
-            className='dropdown-menu dropdown-menu-right shadow animated--grow-in'
-            aria-labelledby='userDropdown'
+            className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+            aria-labelledby="userDropdown"
           >
             <a
-              className='dropdown-item'
-              href='/#'
-              data-toggle='modal'
-              data-target='#logoutModal'
+              className="dropdown-item"
+              href="/#"
+              data-toggle="modal"
+              data-target="#logoutModal"
             >
-              <i className='fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400'></i>
+              <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
               Logout
             </a>
           </div>
