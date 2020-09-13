20ea1758fd244a947902b97c290e12b67943bd0f
muhmmdfrd <mfarid2121@gmail.com>
2020-09-13 19:22:12 +0700

    fix issue expiry employee

--- src/helpers/checkExpiredDate.js
+++ src/helpers/checkExpiredDate.js
@@ -2,8 +2,11 @@ const checkExpiredDate = (data) => {
   const date = new Date(data);
   const currentDate = new Date();
 
-  return date.getMonth() + 1 === currentDate.getMonth() &&
-    date.getFullYear() <= currentDate.getFullYear()
+  return (date.getMonth() + 1 === currentDate.getMonth() &&
+    date.getFullYear() <= currentDate.getFullYear()) ||
+    date.getMonth() === currentDate.getMonth() ||
+    (date.getMonth() <= currentDate.getMonth() &&
+      date.getFullYear() <= currentDate.getFullYear())
     ? { status: "Warning", label: "warning" }
     : { status: "Aktif", label: "success" };
 };
