79620ecc348dcdddaddc479ef5ab878351a62ad2
muhmmdfrd <mfarid2121@gmail.com>
2020-09-15 21:44:29 +0700

    delete late status

--- src/components/Table/PresenceStatisticTable.jsx
+++ src/components/Table/PresenceStatisticTable.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import roundNumber from '../../helpers/roundNumber';
+import React from "react";
+import roundNumber from "../../helpers/roundNumber";
 
 const PresenceStatisticTable = ({ response, postPerPage, currentPage }) => {
   return (
-    <div className='table-responsive'>
-      <table className='table table-striped table-inverse'>
-        <thead className='thead-inverse'>
-          <tr className='d-flex'>
-            <th className='col-1 text-center'>#</th>
-            <th className='col-2'>NIK</th>
-            <th className='col-2'>Nama</th>
-            <th className='col-1'>Zona</th>
-            <th className='col-2'>Bagian</th>
-            <th className='col-1 text-center'>Hadir</th>
-            <th className='col-1 text-center'>Izin</th>
-            <th className='col-1 text-center'>Absen</th>
-            <th className='col-1 text-center'>Telat</th>
+    <div className="table-responsive">
+      <table className="table table-striped table-inverse">
+        <thead className="thead-inverse">
+          <tr className="d-flex">
+            <th className="col-1 text-center">#</th>
+            <th className="col-2">NIK</th>
+            <th className="col-2">Nama</th>
+            <th className="col-2">Zona</th>
+            <th className="col-2">Bagian</th>
+            <th className="col-1 text-center">Hadir</th>
+            <th className="col-1 text-center">Izin</th>
+            <th className="col-1 text-center">Absen</th>
           </tr>
         </thead>
         <tbody>
@@ -28,31 +27,27 @@ const PresenceStatisticTable = ({ response, postPerPage, currentPage }) => {
               presence,
               absence,
               roleName,
-              late,
             } = value;
 
-            const total = presence + leave + absence + late;
+            const total = presence + leave + absence;
 
             return (
-              <tr key={index} className='d-flex'>
-                <td className='col-1 text-center'>
+              <tr key={index} className="d-flex">
+                <td className="col-1 text-center">
                   {(currentPage - 1) * postPerPage + index + 1}
                 </td>
-                <td className='col-2'>{employeeNumber}</td>
-                <td className='col-2'>{employeeName}</td>
-                <td className='col-1'>{zoneName}</td>
-                <td className='col-2'>{roleName}</td>
-                <td className='col-1 text-center'>
+                <td className="col-2">{employeeNumber}</td>
+                <td className="col-2">{employeeName}</td>
+                <td className="col-2">{zoneName}</td>
+                <td className="col-2">{roleName}</td>
+                <td className="col-1 text-center">
                   {roundNumber((presence / total) * 100)}%
                 </td>
-                <td className='col-1 text-center'>
+                <td className="col-1 text-center">
                   {roundNumber((leave / total) * 100)}%
                 </td>
-                <td className='col-1 text-center'>
+                <td className="col-1 text-center">
                   {roundNumber((absence / total) * 100)}%
-                </td>
-                <td className='col-1 text-center'>
-                  {roundNumber((late / total) * 100)}%
                 </td>
               </tr>
             );
