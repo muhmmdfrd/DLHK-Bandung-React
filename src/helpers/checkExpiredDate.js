4e0a2c030b2e6dfd0e9fab578aa094df7062efd5
muhmmdfrd <mfarid2121@gmail.com>
2020-09-22 13:06:39 +0700

    fix for check expired employee

--- src/helpers/checkExpiredDate.js
+++ src/helpers/checkExpiredDate.js
@@ -2,13 +2,17 @@ const checkExpiredDate = (data) => {
   const date = new Date(data);
   const currentDate = new Date();
 
-  return (date.getMonth() + 1 === currentDate.getMonth() &&
-    date.getFullYear() <= currentDate.getFullYear()) ||
-    date.getMonth() === currentDate.getMonth() ||
+  return (
+      date.getMonth() === currentDate.getMonth() + 1 &&
+      date.getFullYear() <= currentDate.getFullYear()) ||
     (date.getMonth() <= currentDate.getMonth() &&
-      date.getFullYear() <= currentDate.getFullYear())
-    ? { status: "Warning", label: "warning" }
-    : { status: "Aktif", label: "success" };
+      date.getFullYear() <= currentDate.getFullYear()) ? {
+      status: "Warning",
+      label: "warning"
+    } : {
+      status: "Aktif",
+      label: "success"
+    };
 };
 
 export default checkExpiredDate;
