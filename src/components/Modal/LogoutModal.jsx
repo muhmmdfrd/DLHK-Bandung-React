ab729667f012e86735a617e06f038d6ebd359596
muhmmdfrd <mfarid2121@gmail.com>
2020-09-22 16:06:13 +0700

    delete tag when user logged out

--- src/components/Modal/LogoutModal.jsx
+++ src/components/Modal/LogoutModal.jsx
@@ -1,48 +1,49 @@
-import React from 'react';
+import React from "react";
 
 const LogoutModal = () => {
   const logOut = (event) => {
     event.preventDefault();
     window.localStorage.clear();
-    window.location.href = '/#/admin/login';
+    window.$(".modal-backdrop.fade.show").remove();
+    window.location.href = "/#/admin/login";
   };
 
   return (
     <div
-      className='modal fade'
-      id='logoutModal'
-      tabIndex='-1'
-      role='dialog'
-      aria-labelledby='exampleModalLabel'
-      aria-hidden='true'
+      className="modal fade"
+      id="logoutModal"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
     >
-      <div className='modal-dialog' role='document'>
-        <div className='modal-content'>
-          <div className='modal-header'>
-            <h5 className='modal-title' id='exampleModalLabel'>
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
               Ready to Leave?
             </h5>
             <button
-              className='close'
-              type='button'
-              data-dismiss='modal'
-              aria-label='Close'
+              className="close"
+              type="button"
+              data-dismiss="modal"
+              aria-label="Close"
             >
-              <span aria-hidden='true'>×</span>
+              <span aria-hidden="true">×</span>
             </button>
           </div>
-          <div className='modal-body'>
+          <div className="modal-body">
             Select "Logout" below if you are ready to end your current session.
           </div>
-          <div className='modal-footer'>
+          <div className="modal-footer">
             <button
-              className='btn btn-secondary'
-              type='button'
-              data-dismiss='modal'
+              className="btn btn-secondary"
+              type="button"
+              data-dismiss="modal"
             >
               Cancel
             </button>
-            <button className='btn btn-danger' onClick={logOut}>
+            <button className="btn btn-danger" onClick={logOut}>
               Logout
             </button>
           </div>
