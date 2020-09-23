a91c3a418293bfdd7d9b8f4f7889041125457504
muhmmdfrd <mfarid2121@gmail.com>
2020-09-23 10:05:43 +0700

    modify view in qr

--- src/pages/ItemQR.jsx
+++ src/pages/ItemQR.jsx
@@ -1,51 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import QR from 'qrcode.react';
+import React, { useState, useEffect } from "react";
+import QR from "qrcode.react";
+import FadeIn from "react-fade-in";
 
 const ItemQR = () => {
-    const [uri, setUri] = useState("");
+  const [uri, setUri] = useState("");
 
-    const getIdUri = (url) => {
-        return url.substring(url.lastIndexOf("/") + 1);
-    };
+  const getIdUri = (url) => {
+    return url.substring(url.lastIndexOf("/") + 1);
+  };
 
-    const handlePrint = () => {
-        // var printContents = window.$('#printarea').val();
-        // var originalContents = document.body.innerHTML;
+  const handlePrint = () => {
+    // var printContents = window.$('#printarea').val();
+    // var originalContents = document.body.innerHTML;
 
-        // document.body.innerHTML = printContents;
+    // document.body.innerHTML = printContents;
 
-        window.print();
+    window.print();
 
-        // document.body.innerHTML = originalContents;
-    }
+    // document.body.innerHTML = originalContents;
+  };
 
-    useEffect(() => {
-        setUri(getIdUri(window.location.href));
-    }, [])
+  useEffect(() => {
+    setUri(getIdUri(window.location.href));
+  }, []);
 
-    return (
+  return (
+    <FadeIn>
+      <div
+        className="container-fluid h-100 pt-4"
+        style={{
+          width: 100 + "%",
+          height: 100 + "%",
+          postition: "relative",
+        }}
+      >
         <div
-            className="container-fluid h-100 pt-4"
-            style={{
-                width: 100 + "%",
-                height: 100 + "%",
-                postition: "relative"
-            }}
+          className="row h-100 justify-content-center align-items-center"
+          id="printarea"
         >
-            <div className="row h-100 justify-content-center align-items-center" id="printarea">
-                <QR
-                    id="qr"
-                    value={uri.replace(/%20/g, " ")}
-                    size={290}
-                    level={"H"}
-                    includeMargin={true}
-                    renderAs={"canvas"}
-                />
-                <button className="m-5 btn btn-primary" onClick={() => handlePrint()}>Print</button>
-                <button className="m-5 btn btn-success" onClick={() => window.location.href = "/#/admin/barang"}>Kembali</button>
-            </div>
+          <QR
+            id="qr"
+            value={uri.replace(/%20/g, " ")}
+            size={290}
+            level={"H"}
+            includeMargin={true}
+            renderAs={"canvas"}
+          />
+          <button
+            className="m-5 btn btn-success"
+            onClick={() => (window.location.href = "/#/admin/barang")}
+          >
+            Kembali
+          </button>
+          <button className="m-5 btn btn-primary" onClick={() => handlePrint()}>
+            Print
+          </button>
         </div>
-    );
-}
+      </div>
+    </FadeIn>
+  );
+};
 
 export default ItemQR;
