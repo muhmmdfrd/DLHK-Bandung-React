746a0e0a5a6cc2a83f0e4f50ee578b1122dccdce
muhmmdfrd <mfarid2121@gmail.com>
2020-10-03 21:59:45 +0700

    add export to pdf in exportPerform

--- src/pages/Export Pages/ExportPerform.jsx
+++ src/pages/Export Pages/ExportPerform.jsx
@@ -1,15 +1,45 @@
 import React, { useContext } from "react";
 import FadeIn from "react-fade-in";
-import checkNullNum from "../../helpers/checkNullNum";
+
+// * context
 import { PresenceContext } from "../../providers/PresenceContext";
+
+// * plugin
 import excel from "tableexport";
+import jsPDF from "jspdf";
+import checkNullNum from "../../helpers/checkNullNum";
+import html2canvas from "html2canvas";
 
 const ExportPerform = () => {
   const { perform, loading } = useContext(PresenceContext);
+  const exportPdf = () => {
+    const form = window.$(".formPerform")[0];
 
-  const exportPdf = () => alert("pdf");
+    getCanvas().then((canvas) => {
+      const img = canvas.toDataURL("image/png");
+      const doc = new jsPDF({
+        unit: "px",
+        format: "a4",
+        orientation: "landscape",
+      });
+
+      doc.addImage(img, "JPEG", 20, 20);
+      doc.save("Statistik-Performa.pdf");
+    });
+
+    function getCanvas() {
+      return html2canvas(form, {
+        imageTimeout: 2000,
+        removeContainer: true,
+      });
+    }
+  };
 
   const exportExcel = () => {
+    excel.prototype.typeConfig.date.assert = () => {
+      return false;
+    };
+
     excel(window.$("#tablePerform"), {
       formats: ["xlsx"],
       bootstrap: true,
@@ -42,47 +72,52 @@ const ExportPerform = () => {
         </div>
       </div>
 
-      <div id="tablePerform" className="table-responsive">
-        <table className="table table-striped table-inverse">
-          <thead className="thead-inverse">
-            <tr className="d-flex">
-              <th className="col-1 text-center">#</th>
-              <th className="col-2">NIP</th>
-              <th className="col-2">Nama</th>
-              <th className="col-2">Pekerjaan</th>
-              <th className="col-2">Wilayah</th>
-              <th className="col-1">Zona</th>
-              <th className="col-2 text-center">Persentase Performa</th>
-            </tr>
-          </thead>
-          <tbody>
-            {perform.map((value, index) => {
-              const {
-                employeeNumber,
-                employeeName,
-                regionName,
-                zoneName,
-                roleName,
-                percentage,
-              } = value;
+      <form
+        class="formPerform"
+        style={{ maxWidth: "none", width: 1005 + "px" }}
+      >
+        <div id="tablePerform" className="table-responsive">
+          <table className="table table-striped table-inverse">
+            <thead className="thead-inverse">
+              <tr className="d-flex">
+                <th className="col-1 text-center">#</th>
+                <th className="col-2">NIP</th>
+                <th className="col-2">Nama</th>
+                <th className="col-2">Pekerjaan</th>
+                <th className="col-2">Wilayah</th>
+                <th className="col-1">Zona</th>
+                <th className="col-2 text-center">Persentase Performa</th>
+              </tr>
+            </thead>
+            <tbody>
+              {perform.map((value, index) => {
+                const {
+                  employeeNumber,
+                  employeeName,
+                  regionName,
+                  zoneName,
+                  roleName,
+                  percentage,
+                } = value;
 
-              return (
-                <tr key={index} className="d-flex">
-                  <td className="col-1 text-center">{index + 1}</td>
-                  <td className="col-2">{employeeNumber}</td>
-                  <td className="col-2">{employeeName}</td>
-                  <td className="col-2">{roleName}</td>
-                  <td className="col-2">{regionName}</td>
-                  <td className="col-1">{zoneName.toString()}</td>
-                  <td className="col-2 text-center">
-                    {checkNullNum(percentage)}%
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+                return (
+                  <tr key={index} className="d-flex">
+                    <td className="col-1 text-center">{index + 1}</td>
+                    <td className="col-2">{employeeNumber}</td>
+                    <td className="col-2">{employeeName}</td>
+                    <td className="col-2">{roleName}</td>
+                    <td className="col-2">{regionName}</td>
+                    <td className="col-1">{zoneName.toString()}</td>
+                    <td className="col-2 text-center">
+                      {checkNullNum(percentage)}%
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </form>
     </FadeIn>
   );
 };
--- src/pages/Export Pages/ExportPresence.jsx
+++ src/pages/Export Pages/ExportPresence.jsx
@@ -51,58 +51,60 @@ const ExportPresence = () => {
         </div>
       </div>
 
-      <div id="tablePresence" className="table-responsive">
-        <table className="table table-striped table-inverse">
-          <thead className="thead-inverse">
-            <tr className="d-flex">
-              <th className="col-1 text-center">#</th>
-              <th className="col-2">NIK</th>
-              <th className="col-2">Nama</th>
-              <th className="col-2">Zona</th>
-              <th className="col-2">Bagian</th>
-              <th className="col-1 text-center">Hadir</th>
-              <th className="col-1 text-center">Izin</th>
-              <th className="col-1 text-center">Absen</th>
-            </tr>
-          </thead>
-          <tbody>
-            {presence.map((value, index) => {
-              const {
-                employeeNumber,
-                employeeName,
-                zoneName,
-                leave,
-                presence,
-                absence,
-                roleName,
-              } = value;
+      <form class="form" style={{ maxWidth: "none" }}>
+        <div id="tablePresence" className="table-responsive">
+          <table className="table table-striped table-inverse">
+            <thead className="thead-inverse">
+              <tr className="d-flex">
+                <th className="col-1 text-center">#</th>
+                <th className="col-2">NIK</th>
+                <th className="col-2">Nama</th>
+                <th className="col-2">Zona</th>
+                <th className="col-2">Bagian</th>
+                <th className="col-1 text-center">Hadir</th>
+                <th className="col-1 text-center">Izin</th>
+                <th className="col-1 text-center">Absen</th>
+              </tr>
+            </thead>
+            <tbody>
+              {presence.map((value, index) => {
+                const {
+                  employeeNumber,
+                  employeeName,
+                  zoneName,
+                  leave,
+                  presence,
+                  absence,
+                  roleName,
+                } = value;
 
-              const total = presence + leave + absence;
+                const total = presence + leave + absence;
 
-              return loading ? (
-                <p>Mohon Tunggu</p>
-              ) : (
-                <tr key={index} className="d-flex">
-                  <td className="col-1 text-center">{index + 1}</td>
-                  <td className="col-2">{employeeNumber}</td>
-                  <td className="col-2">{employeeName}</td>
-                  <td className="col-2">{zoneName.toString()}</td>
-                  <td className="col-2">{roleName}</td>
-                  <td className="col-1 text-center">
-                    {roundNumber((presence / total) * 100)}%
-                  </td>
-                  <td className="col-1 text-center">
-                    {roundNumber((leave / total) * 100)}%
-                  </td>
-                  <td className="col-1 text-center">
-                    {roundNumber((absence / total) * 100)}%
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+                return loading ? (
+                  <p>Mohon Tunggu</p>
+                ) : (
+                  <tr key={index} className="d-flex">
+                    <td className="col-1 text-center">{index + 1}</td>
+                    <td className="col-2">{employeeNumber}</td>
+                    <td className="col-2">{employeeName}</td>
+                    <td className="col-2">{zoneName.toString()}</td>
+                    <td className="col-2">{roleName}</td>
+                    <td className="col-1 text-center">
+                      {roundNumber((presence / total) * 100)}%
+                    </td>
+                    <td className="col-1 text-center">
+                      {roundNumber((leave / total) * 100)}%
+                    </td>
+                    <td className="col-1 text-center">
+                      {roundNumber((absence / total) * 100)}%
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </form>
     </FadeIn>
   );
 };
